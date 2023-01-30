614e21c91580e3a1b5b34baf33666709548d7593
Joseph Milazzo <joseph.v.milazzo@gmail.com>
2023-01-30 03:22:40 -0600

    Upgraded project to .NET 7

--- KavitaEmail/Extensions/ApplicationServiceExtensions.cs
+++ KavitaEmail/Extensions/ApplicationServiceExtensions.cs
@@ -27,7 +27,7 @@ namespace Skeleton.Extensions
         }
 
         private static void AddSqLite(this IServiceCollection services, IConfiguration config,
-            IWebHostEnvironment env)
+            IHostEnvironment env)
         {
             services.AddDbContext<DataContext>(options =>
             {
