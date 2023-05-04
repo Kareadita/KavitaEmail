652c4ba3c0c59de3b5b7f65e3293af0dcb4e67b8
Chris P <kizaing@gmail.com>
2023-05-04 11:17:40 -0400

    Fixed templates not copying

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,18 @@ if [ ! -f "/app/config/appsettings.json" ]; then
     fi
 fi
 
+#Checks if the templates folder exists, and creates it if it does not
+if [ ! -d "/app/config/templates" ]; then
+    echo "Templates folder does not exist, copying from temp..."
+    cp -r /tmp/templates /app/config/templates
+    if [ -d "/app/config/templates" ]; then
+        echo "Copy completed successfully, starting app..."
+    else
+        echo "Copy failed, check folder permissions. Exiting..."
+        exit
+    fi
+fi
+
 chmod +x KavitaEmail
 
 ./KavitaEmail
